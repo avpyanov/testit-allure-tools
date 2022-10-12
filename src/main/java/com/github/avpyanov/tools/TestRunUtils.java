e3608435bba08535ea0b930f1b65b42727cb831f
apyanov <>
2022-10-12 18:17:01 +0300

    fix configurationId

--- src/main/java/com/github/avpyanov/tools/TestRunUtils.java
+++ src/main/java/com/github/avpyanov/tools/TestRunUtils.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class TestRunUtils {
 
     public static String getConfigurationId(String testRunId){
-        return AllureConfig.getTestItApiClient().testRunsApi().getTestRun(testRunId).getTestResults().get(0).getId();
+        return AllureConfig.getTestItApiClient().testRunsApi().getTestRun(testRunId).getTestResults().get(0).getConfiguration().getId();
     }
     public static void uploadTestResults(String testRunId, List<AutotestResults> autotestResults){
         AllureConfig.getTestItApiClient().testRunsApi().setAutoTestsResults(testRunId,autotestResults);
